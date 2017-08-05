4d1d7488fcd7b7f55db27ceb2ec8761ab1542619
Kuxika Chamoru <kuxika.chamoru@gmail.com>
2017-08-05 13:40:56 +1000

    updated buncha crap; added userpass functionality with a gate.  needs lost of cleanup.

--- app/src/main/java/gu/coconutservice/ppl/consoletester/ConsoleActivity.java
+++ app/src/main/java/gu/coconutservice/ppl/consoletester/ConsoleActivity.java
@@ -2,12 +2,60 @@ package gu.coconutservice.ppl.consoletester;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ConsoleActivity extends AppCompatActivity {
+
+    String username;
+    String password;
+    String[] listViewItems_STRING;// = {"Bacon", "Eggs", "Potatos", "Waffles", "More", "Food", "Names", "Because", "Scrolling", "Might", "Be", "An", "Issue"};
+    List<String> listViewItems_LIST;
+    ArrayAdapter<String> listAdapter;
+    ListView listView;
+    Button submitButton;
+    EditText inputBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_console);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            username = getIntent().getStringExtra("USERNAME");
+            password = getIntent().getStringExtra("PASSWORD");
+            // create a new String[] array
+            listViewItems_STRING = new String[] {"Bacon", "Eggs", "Potatos", "Waffles", "More", "Food", "Names", "Because", "Scrolling", "Might", "Be", "An", "Issue"};
+            // create a list from the String[] array
+            listViewItems_LIST = new ArrayList<String>(Arrays.asList(listViewItems_STRING));
+            // create an ArrayAdapter from the list
+            listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listViewItems_LIST);
+            listView = (ListView) findViewById(R.id.listView);
+            listView.setAdapter(listAdapter);
+            //listView.setSelection(listAdapter.getCount() - 1);
+        } else {
+            // do nothing
+        }
+
+        submitButton = (Button)findViewById(R.id.submitButton);
+        inputBar = (EditText)findViewById(R.id.editText);
+        submitButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                listViewItems_LIST.add(inputBar.getText().toString());
+                listAdapter.notifyDataSetChanged();
+                listView.setSelection(listAdapter.getCount() - 1);
+                inputBar.setText("");
+            }
+        });
+
     }
 }
--- app/src/main/java/gu/coconutservice/ppl/consoletester/MainActivity.java
+++ app/src/main/java/gu/coconutservice/ppl/consoletester/MainActivity.java
@@ -3,16 +3,19 @@ package gu.coconutservice.ppl.consoletester;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText usernameField;
     EditText passwordField;
     Button loginButton;
-    Intent switchTo_ConsoleActivity;
+    Intent intent_switchTo_ConsoleActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +24,29 @@ public class MainActivity extends AppCompatActivity {
         usernameField = (EditText) findViewById(R.id.usernameField);
         passwordField = (EditText) findViewById(R.id.passwordField);
         loginButton = (Button) findViewById(R.id.login_button);
-        switchTo_ConsoleActivity = new Intent(this, ConsoleActivity.class); // can also use getBaseContext() in place of "this"
+        intent_switchTo_ConsoleActivity = new Intent(this, ConsoleActivity.class); // can also use getBaseContext() in place of "this"
+        //intent_switchTo_ConsoleActivity.putExtra("USERNAME", "admin");
+        //intent_switchTo_ConsoleActivity.putExtra("PASSWORD", "idiot212");
         loginButton.setOnClickListener(new View.OnClickListener() {
             public void onClick (View view) {
-                MainActivity.this.startActivity(switchTo_ConsoleActivity);
+                //Log.i("appLogs", "geez");
+                if (usernameField.getText().toString().trim().length() == 0 || passwordField.getText().toString().trim().length() == 0){
+                    Toast badToast = Toast.makeText(getApplicationContext(), "Enter username and password, dimwit.", Toast.LENGTH_LONG);
+                    badToast.show();
+                    //Log.i("appLogs", usernameField.toString());
+                    //intent_switchTo_ConsoleActivity.putExtra("USERNAME", usernameField.getText().toString());
+                    //intent_switchTo_ConsoleActivity.putExtra("PASSWORD", passwordField.getText().toString());
+                    //MainActivity.this.startActivity(intent_switchTo_ConsoleActivity);
+                    //finish();
+                }else{
+                    //String username = usernameField.getText().toString();
+                    //String password = passwordField.getText().toString();
+                    //Toast toast = Toast.makeText(getApplicationContext(), "Username: " + username + " Password: " + password, Toast.LENGTH_LONG);
+                    //toast.show();
+                    intent_switchTo_ConsoleActivity.putExtra("USERNAME", usernameField.getText().toString());
+                    intent_switchTo_ConsoleActivity.putExtra("PASSWORD", passwordField.getText().toString());
+                    MainActivity.this.startActivity(intent_switchTo_ConsoleActivity);
+                }
                 // finish(); can kill the activity and prevent user from returning to it
                 //usernameField.setVisibility(View.GONE);
                 //passwordField.setVisibility(View.GONE);
